d6d67e333c950067fe34978c6ef9cbfe963fc01e
6ninjamod <6ninjamod786@gmail.com>
2025-02-08 22:07:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7548324885:AAGcbXu9n5yb333rHOVzIc6zDBi7fgOcsZs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("NINJAGAMEROP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1240179115")
+
+
+
+
+
+
+
+  
